d2d0b28a17cfe989967d79e209bfaef63deb0554
Patra Raples Wojtyla Sinaga <rapleswojtyla@gmail.com>
2025-08-08 15:30:54 +0700

    bismillah: fix(images): Correct next/image config to display Strapi images

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,11 +4,20 @@ import { RemotePattern } from "next/dist/shared/lib/image-config";
 const strapiUrlString = process.env.STRAPI_API_URL || 'http://localhost:1337'
 const strapiUrl = new URL(strapiUrlString)
 
+const strapiMediaUrlString = process.env.STRAPI_MEDIA_URL || 'http://localhost:1337'
+const strapiMediaUrl = new URL(strapiMediaUrlString)
+
 const remotePatterns: RemotePattern[] = [
   {
     protocol: strapiUrl.protocol.replace(':', '') as 'http' | 'https' | undefined,
     hostname: strapiUrl.hostname,
     port: strapiUrl.port,
+    pathname: '/**'
+  },
+  {
+    protocol: strapiMediaUrl.protocol.replace(':', '') as 'http' | 'https',
+    hostname: strapiMediaUrl.hostname,
+    port: strapiMediaUrl.port,
     pathname: '/**'
   }
 ]
