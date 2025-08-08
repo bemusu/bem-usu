e18c0c84cfc7c85f49274ddfcbc375bd00856e3a
Patra Raples Wojtyla Sinaga <rapleswojtyla@gmail.com>
2025-08-08 15:03:20 +0700

    test: image

--- client/src/components/StrapiImage.tsx
+++ client/src/components/StrapiImage.tsx
@@ -17,21 +17,24 @@ const StrapiImage = ({
 }: Readonly<StrapiImageProps>) => {
     const imgUrl = getStrapiMedia(src)
     if (!imgUrl) return null
-    
+
     return (
-        <Image 
-            src={imgUrl}
-            className={className}
-            alt={alt || 'gambar-'}
-            {...rest}
-        />
+        <>
+            <p className="text-black text-lg absolute z-10">{imgUrl}</p>
+            <Image
+                src={imgUrl}
+                className={className}
+                alt={alt || 'gambar-'}
+                {...rest}
+            />
+        </>
     )
 }
 
 export const getStrapiMedia = (url: string | null) => {
     if (url === null) return null
     if (url.startsWith("data:")) return url
-    if (url.startsWith("http") || url.startsWith("//")) return  url
+    if (url.startsWith("http") || url.startsWith("//")) return url
 
     return getStrapiUrl() + url
 }
